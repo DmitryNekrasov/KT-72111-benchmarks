97e106bf691f4685f4ec0f912a7d3267c7a77719
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-07-08 16:58:31 +0400

    Add additional `iso_fail` test cases to `DurationParserBenchmarks` for improved coverage of invalid duration formats.

--- src/commonMain/kotlin/benchmark/duration/DurationParserBenchmarks.kt
+++ src/commonMain/kotlin/benchmark/duration/DurationParserBenchmarks.kt
@@ -23,7 +23,17 @@ open class DurationParseBenchmark {
         "new_iso_PT_only", "new_iso_week", "new_iso_dup_sec",
         "new_iso_unknown_unit", "new_def_unknown_unit",
         "new_def_fraction_not_last", "new_def_dup_unit",
-        "new_bad_paren", "new_inf_suffix", "new_inf_short"
+        "new_bad_paren", "new_inf_suffix", "new_inf_short",
+        "iso_fail_empty", "iso_fail_space", "iso_fail_P", "iso_fail_PT", "iso_fail_P1DT",
+        "iso_fail_P1", "iso_fail_PT1", "iso_fail_0", "iso_fail_plusP", "iso_fail_plus",
+        "iso_fail_minus", "iso_fail_h", "iso_fail_H", "iso_fail_something",
+        "iso_fail_1m", "iso_fail_1d", "iso_fail_2d11s", "iso_fail_infinity",
+        "iso_fail_minusInfinity", "iso_fail_Pplus12plus34D", "iso_fail_P12minus34D",
+        "iso_fail_bigNegSecs", "iso_fail_spaceP1D", "iso_fail_PT1S_space", "iso_fail_P3W",
+        "iso_fail_P1Y", "iso_fail_P1M", "iso_fail_P1S", "iso_fail_PT1D", "iso_fail_PT1Y",
+        "iso_fail_PT1S2S", "iso_fail_PT1S2H", "iso_fail_bigRange",
+        "iso_fail_PT1_5H", "iso_fail_PT0_5D", "iso_fail_PT_dot5S", "iso_fail_PT0_25_25S",
+        "iso_fail_PT_plusminus2H", "iso_fail_PT_minusplus2H", "iso_fail_PT_plusminusLongS"
     )
     lateinit var caseId: String
 
@@ -73,6 +83,48 @@ open class DurationParseBenchmark {
             "new_bad_paren" -> "-(12m 30s"
             "new_inf_suffix" -> "Infinity value"
             "new_inf_short" -> "Inf"
+
+            // Mappings for iso_fail
+            "iso_fail_empty" -> ""
+            "iso_fail_space" -> " "
+            "iso_fail_P" -> "P"
+            "iso_fail_PT" -> "PT"
+            "iso_fail_P1DT" -> "P1DT"
+            "iso_fail_P1" -> "P1"
+            "iso_fail_PT1" -> "PT1"
+            "iso_fail_0" -> "0"
+            "iso_fail_plusP" -> "+P"
+            "iso_fail_plus" -> "+"
+            "iso_fail_minus" -> "-"
+            "iso_fail_h" -> "h"
+            "iso_fail_H" -> "H"
+            "iso_fail_something" -> "something"
+            "iso_fail_1m" -> "1m"
+            "iso_fail_1d" -> "1d"
+            "iso_fail_2d11s" -> "2d 11s"
+            "iso_fail_infinity" -> "Infinity"
+            "iso_fail_minusInfinity" -> "-Infinity"
+            "iso_fail_Pplus12plus34D" -> "P+12+34D"
+            "iso_fail_P12minus34D" -> "P12-34D"
+            "iso_fail_bigNegSecs" -> "PT1234567890-1234567890S"
+            "iso_fail_spaceP1D" -> " P1D"
+            "iso_fail_PT1S_space" -> "PT1S "
+            "iso_fail_P3W" -> "P3W"
+            "iso_fail_P1Y" -> "P1Y"
+            "iso_fail_P1M" -> "P1M"
+            "iso_fail_P1S" -> "P1S"
+            "iso_fail_PT1D" -> "PT1D"
+            "iso_fail_PT1Y" -> "PT1Y"
+            "iso_fail_PT1S2S" -> "PT1S2S"
+            "iso_fail_PT1S2H" -> "PT1S2H"
+            "iso_fail_bigRange" -> "P9999999999999DT-9999999999999H"
+            "iso_fail_PT1_5H" -> "PT1.5H"
+            "iso_fail_PT0_5D" -> "PT0.5D"
+            "iso_fail_PT_dot5S" -> "PT.5S"
+            "iso_fail_PT0_25_25S" -> "PT0.25.25S"
+            "iso_fail_PT_plusminus2H" -> "PT+-2H"
+            "iso_fail_PT_minusplus2H" -> "PT-+2H"
+            "iso_fail_PT_plusminusLongS" -> "PT+-01234567890123456S"
 
             else -> error("Unhandled case-id: $caseId")
         }
