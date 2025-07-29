95e6461c2b032d9a937e9d3b0839334349d18626
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-07-29 15:54:56 +0400

    Add `DurationParseISOBenchmark` to evaluate ISO duration parsing performance

---
+++ src/commonMain/kotlin/benchmark/duration/DurationParseISOBenchmark.kt
@@ -0,0 +1,60 @@
+@file:Suppress("unused")
+
+package benchmark.duration
+
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Blackhole
+import kotlinx.benchmark.Param
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.State
+import kotlin.time.Duration
+
+@State(Scope.Benchmark)
+open class DurationParseISOBenchmark {
+    @Param(
+        // Successful cases
+        "len3",
+        "len5",
+        "len8",
+        "len13",
+        "len21",
+        "len34",
+        "len55",
+
+        // Failing cases
+        "invalid_empty",
+        "iso_wrong_order",
+        "iso_bad_fraction"
+    )
+    lateinit var caseId: String
+
+    val input: String
+        get() = when (caseId) {
+            // Successful cases
+            "len3" -> "P1D"
+            "len5" -> "PT24H"
+            "len8" -> "P1DT140M"
+            "len13" -> "P123DT10H123S"
+            "len21" -> "P15DT1234H128M52.874S"
+            "len34" -> "P1234DT2587H85471M1234567.5258741S"
+            "len55" -> "P1000DT123456708H875412386098M125487514523.25807451235S"
+
+            // Failing cases
+            "invalid_empty" -> ""
+            "iso_wrong_order" -> "PT1M2H"
+            "iso_bad_fraction" -> "PT0.25.25S"
+
+            else -> error("Unhandled case-id: $caseId")
+        }
+
+    val durationWrapper = DurationWrapper(Duration.ZERO)
+
+    @Benchmark
+    fun parse(bh: Blackhole) {
+        try {
+            durationWrapper.duration = Duration.parse(input)
+            bh.consume(durationWrapper)
+        } catch (_: IllegalArgumentException) {
+        }
+    }
+}
